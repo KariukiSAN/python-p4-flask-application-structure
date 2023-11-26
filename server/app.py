4a220bede88ecc822c11cb8cca762607b0143be2
KariukiSAN <kariukian1@gmail.com>
2023-11-26 19:12:02 +0300

    104Comp

--- server/app.py
+++ server/app.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Welcome to the Home Page!</h1>'
+
+@app.route('/<username>')
+def user(username):
+    return f'<h1>This is {username}\'s profile.</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
